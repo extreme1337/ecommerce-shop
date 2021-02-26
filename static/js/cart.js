2a346ef6e57daaffc99975235f72161d53ebeb8c
Marko Miseljic <marko.miseljic.14@gmail.com>
2021-02-26 13:29:42 +0100

    shopping cart and checkout for guest user

--- static/js/cart.js
+++ static/js/cart.js
@@ -29,14 +29,35 @@ function updateUserOrder(productId, action) {
             body: JSON.stringify({ 'productId': productId, 'action': action })
         })
         .then((response) => {
-            if (!response.ok) {
-                // error processing
-                throw 'Error';
-            }
-            return response.json()
+            return response.json();
         })
         .then((data) => {
-            console.log('data:', data)
             location.reload()
-        })
+        });
+}
+
+function addCookieItem(productId, action) {
+    console.log('User is not authenticated')
+
+    if (action == 'add') {
+        if (cart[productId] == undefined) {
+            cart[productId] = { 'quantity': 1 }
+
+        } else {
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action == 'remove') {
+        cart[productId]['quantity'] -= 1
+
+        if (cart[productId]['quantity'] <= 0) {
+            console.log('Item should be deleted')
+            delete cart[productId];
+        }
+    }
+    console.log('CART:', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+
+    location.reload()
 }
